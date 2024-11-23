19c2e19ec06697ce319b76a18676489f985a9fad
Gladson Routh <grauth65@gmail.com>
2024-11-23 23:02:00 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -66,6 +66,6 @@ const appRouter = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={appRouter}/>
+    <RouterProvider router={appRouter} />
   </StrictMode>,
 )
