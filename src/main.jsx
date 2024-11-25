c6b3d487f3a6489b31e02d0022b1a2a6f1d7ad24
Gladson Routh <grauth65@gmail.com>
2024-11-25 14:57:31 +0530

    modified

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ const appRouter = createBrowserRouter([
     element: <App />,
     children: [
       {
-        path: "/",
+        path: "/home",
         element: (
           <Suspense fallback={<h1>Loading...</h1>}>
             <Home />
